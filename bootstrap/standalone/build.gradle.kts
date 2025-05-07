95c5551f6cc1382fe9b81b10454e6c363938ac9d
virtualspan <108852984+virtualspan@users.noreply.github.com>
2025-05-07 11:24:04 +0000

    Upload bootstrap Part 1

---
+++ bootstrap/standalone/build.gradle.kts
@@ -0,0 +1,45 @@
+import com.github.jengelman.gradle.plugins.shadow.transformers.Log4j2PluginsCacheFileTransformer
+
+plugins {
+    application
+    id("geyser.platform-conventions")
+}
+
+dependencies {
+    api(projects.core)
+
+    implementation(libs.terminalconsoleappender) {
+        exclude("org.apache.logging.log4j")
+        exclude("org.jline")
+    }
+
+    implementation(libs.bundles.jline)
+
+    implementation(libs.bundles.log4j)
+}
+
+application {
+    mainClass.set("org.geysermc.geyser.platform.standalone.GeyserStandaloneBootstrap")
+}
+
+tasks.named<Jar>("jar") {
+    manifest {
+        // log4j provides multi-release java 9 code which resolves https://github.com/GeyserMC/Geyser/issues/3693
+        attributes("Multi-Release" to true)
+    }
+}
+
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    archiveBaseName.set("Geyser-Standalone")
+
+    transform(Log4j2PluginsCacheFileTransformer())
+}
+
+tasks.named<JavaExec>("run") {
+    val dir = projectDir.resolve("run")
+    dir.mkdirs()
+    jvmArgs("-Dio.netty.leakDetection.level=PARANOID")
+    workingDir = dir
+
+    standardInput = System.`in`
+}
